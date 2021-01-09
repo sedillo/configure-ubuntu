02bc1780cb6d729288144a72394139a18928cce5
Mike Sedillo <mike.sedillo@gmail.com>
2021-01-09 14:44:31 -0700

    Forcing install for dpkg

--- install-mega.sh
+++ install-mega.sh
@@ -1,5 +1,6 @@
 wget https://mega.nz/linux/MEGAsync/xUbuntu_20.04/amd64/megasync-xUbuntu_20.04_amd64.deb
-dpkg -i megasync-xUbuntu_20.04_amd64.deb
+#https://askubuntu.com/questions/104493/errors-were-encountered-while-processing-man-db
+dpkg -i --force-all megasync-xUbuntu_20.04_amd64.deb
 apt-get install -f -y
 
 
