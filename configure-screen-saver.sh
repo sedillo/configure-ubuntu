08d5643534ddeaf5508c15cd2e19ddbb25950d08
Mike Sedillo <Mike.Sedillo@intel.com>
2021-01-13 13:31:34 -0700

    vim updates

--- configure-screen-saver.sh
+++ configure-screen-saver.sh
@@ -2,10 +2,12 @@
 
 #https://ubuntu-mate.community/t/stop-the-screen-from-blanking-in-ubuntu/678
 #xset -dpms s off s noblank s 0 0 s noexpose
-
+#https://askubuntu.com/questions/138918/how-do-i-disable-the-blank-console-screensaver-on-ubuntu-server
 #https://livingthelinuxlifestyle.wordpress.com/2019/08/19/disable-screen-blanking-in-linux/
 xset dpms 0 0 0 && xset -dpms  && xset s off && xset s noblank
 
-
-#https://askubuntu.com/questions/138918/how-do-i-disable-the-blank-console-screensaver-on-ubuntu-server
+#https://superuser.com/a/1469489
+gsettings set org.gnome.desktop.lockdown disable-lock-screen 'true'
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 
