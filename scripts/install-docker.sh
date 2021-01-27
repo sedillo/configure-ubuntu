baa474663e30e979d56f0365560dc3efde1f96a9
Mike Sedillo <Mike.Sedillo@gmail.com>
2021-01-27 16:00:51 -0700

    Minor changes

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -9,4 +9,5 @@ chmod +x /tmp/docker-install.sh
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+sudo usermod -aG docker $USER
 
