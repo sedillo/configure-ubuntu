aa990e0157426ef393feb26042127bb453b488b5
Mike Sedillo <Mike.Sedillo@gmail.com>
2021-01-29 14:18:00 -0700

    Cleaning up docker script

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 #Install docker
-wget get.docker.io -O /tmp/docker-install.sh
-chmod +x /tmp/docker-install.sh
-/tmp/docker-install.sh
+if ! command -v docker &> /dev/null; then
+    wget get.docker.io -O /tmp/docker-install.sh
+    chmod +x /tmp/docker-install.sh
+    /tmp/docker-install.sh
+    sudo usermod -aG docker $USER
+else
+    echo "docker already exists"
+fi
+
 
 #Install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+if ! command -v docker-compose &> /dev/null; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+else
+    echo "docker-compose already exists"
+fi
 
-sudo usermod -aG docker $USER
 
